dcf473c07d7167a97e8b1d7826e5c6ce00d1b584
MoonjaeChoi <pro301@empal.com>
2023-06-08 08:05:20 +0900

    RecordView

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import React from 'react'
-//import ScreenRecording from './components/ScreenRecording'
-
 import dynamic from "next/dynamic";
-
+//import ScreenRecording from './components/ScreenRecording'
 const ScreenRecording = dynamic(() => import('./components/ScreenRecording'))
+
+//import RecordView from './components/RecordView';
+const RecordView = dynamic(() => import('./components/RecordView'))
 
 function Home() {
   return (
     <div className='content'>
-      <ScreenRecording
+      {/* <ScreenRecording
         screen={true}
         audio={false}
         video={false}
         downloadRecordingPath="Screen_Recording_Demo"
         downloadRecordingType="mp4"
         emailToSupport="pro301@empal.com"
-      ></ScreenRecording>
+      ></ScreenRecording> */}
+      <RecordView />
     </div>
   )
 }
